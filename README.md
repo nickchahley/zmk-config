634225d083886f5fa9dbc9764c7a4b84611ba363
nickchahley <nickchahley@gmail.com>
2024-03-03 13:35:59 -0500

    Allow local build

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # zmk-config
 [ZMK firmware](https://github.com/zmkfirmware/zmk/) config for the [Swoop MX](https://github.com/jimmerricks/swoop).
 
+## requires
+- install zmk tooling
+- install zephyr-sdk (likely to `~/.local/zephyr-sdk`)
+- initialize submodule: zmk-nodefree-config (`git submodule init`)
+
 ## Features
 - [Miryoku](https://github.com/manna-harbour/miryoku) keyboard layout
-- [urob's timeless homerow mods](https://github.com/urob/zmk-config#timeless-homerow-mods)
+- [urob's timeless homerow mods](https://github.com/urob/zmk-config#timeless-homerow-mods) (`config/includes`)
 
 ## Todo
 - Add mouse emulation layer
---
+++ local-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+zmk_app="/home/nikoli/personal/keyboard/zmk-firmware/zmk/app"
+build_base="/home/nikoli/personal/keyboard/zmk-firmware/miryoku-zmk-swoop/build/swoop"
+zmk_config="/home/nikoli/personal/keyboard/zmk-firmware/miryoku-zmk-swoop/config"
+board="nice_nano_v2"
+
+build_zmk () {
+	west build --build-dir "${build_base}/$1" -p -b "$board" -- -DZMK_CONFIG="$zmk_config" -DSHIELD="$1"
+	cp -v "${build_base}/$1/zephyr/zmk.uf2" "${build_base}/artifacts/$1.uf2"
+}
+
+# must be run from within zmk/app
+cd "$zmk_app"
+
+# stop if somethings fails to build 
+build_zmk swoop_right && build_zmk swoop_left && build_zmk settings_reset
+
+cd -
